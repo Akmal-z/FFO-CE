22f078f6a81c3fe0eebbc9a8d0ae27cc0240e39a
Akmal-z <akmalzuhairi.az@gmail.com>
2026-01-13 14:40:19 +0800

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import time
 import matplotlib.pyplot as plt
 
 #config
-st.title(" Employee Shift Scheduling (FFO)🐜 ")
+st.title(" Employee Shift Scheduling (FFO) ")
 
 n_departments = 6
 n_days = 7
